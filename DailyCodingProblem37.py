2d355b055c3ea38a2b34b51ef3188db46b51152b
xdaniel52 <xdaniel@onet.eu>
2022-08-22 18:13:24 +0200

    Add DCP 37

---
+++ DailyCodingProblem37.py
@@ -0,0 +1,22 @@
+"""
+This problem was asked by Google.
+
+The power set of a set is the set of all its subsets. Write a function that, given a set, generates its power set.
+
+For example, given the set {1, 2, 3}, it should return {{}, {1}, {2}, {3}, {1, 2}, {1, 3}, {2, 3}, {1, 2, 3}}.
+
+You may also use a list or array to represent a set.
+"""
+
+def GetPowerSet(given_set : set):
+    result = [set()]
+    for el in given_set:
+        for i in range(len(result)):
+            tmp = result[i].copy()
+            tmp.add(el)
+            result.append(tmp)
+    return result
+
+
+print(GetPowerSet({1,2,3}))
+
