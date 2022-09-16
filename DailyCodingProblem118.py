f6f0d14e09a71ca2a89b2a4b0a1d9995fee8c071
xdaniel52 <xdaniel@onet.eu>
2022-09-16 21:56:37 +0200

    Add DCP 118

---
+++ DailyCodingProblem118.py
@@ -0,0 +1,34 @@
+"""
+This problem was asked by Google.
+
+Given a sorted list of integers, square the elements and give the output in sorted order.
+
+For example, given [-9, -2, 0, 2, 3], return [0, 4, 4, 9, 81].
+"""
+
+def SquareList(sorted_list):
+    result_list = []
+    add_idx = 0
+    for e in sorted_list[:]:
+        squared = e*e
+        if e <= 0:
+            result_list.insert(0, squared)
+        else:         
+            while True:
+                if squared <= result_list[add_idx]:
+                    result_list.insert(add_idx, squared)
+                    break
+                else:
+                    add_idx+=1
+                    if add_idx == len(result_list):
+                        result_list.append(squared)                      
+                        break            
+    return result_list
+
+
+sorted_list = [-9, -2, 0, 2, 3]
+print(SquareList(sorted_list))
+
+
+sorted_list = [-100,-9,-5, -2, 0, 6, 8, 101]
+print(SquareList(sorted_list))
