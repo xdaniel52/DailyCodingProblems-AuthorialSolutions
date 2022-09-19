334c179be7d02586c8d88a45514a8f0e4e0def7a
xdaniel52 <xdaniel@onet.eu>
2022-09-19 18:54:33 +0200

    Add DCP 157

---
+++ DailyCodingProblem157.py
@@ -0,0 +1,33 @@
+"""
+This problem was asked by Amazon.
+
+Given a string, determine whether any permutation of it is a palindrome.
+
+For example, carrace should return true, since it can be rearranged to form racecar, 
+which is a palindrome. daily should return false, since there's no rearrangement that can form a palindrome.
+"""
+
+def Determine(string):
+    letter_dict = {}
+    for l in string:
+        if l in letter_dict.keys():
+            letter_dict[l]+=1
+        else:
+            letter_dict[l]=1
+
+    odd_finded =True if len(string)%2 == 0 else False
+
+    for val in letter_dict.values():
+        if val%2 == 1:
+            if odd_finded:
+                return False
+            else:
+                odd_finded = True
+    return True            
+
+
+string1 = "carrace"
+print(Determine(string1))
+
+string2 = "daily"
+print(Determine(string2))
