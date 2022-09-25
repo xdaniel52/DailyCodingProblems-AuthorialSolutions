b24dd549623fd24a49ee0cde62e3a3810f2b1fdb
xdaniel52 <xdaniel@onet.eu>
2022-09-25 22:32:12 +0200

    Add DCP 173

---
+++ DailyCodingProblem173.py
@@ -0,0 +1,50 @@
+"""
+This problem was asked by Stripe.
+
+Write a function to flatten a nested dictionary. Namespace the keys with a period.
+
+For example, given the following dictionary:
+
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+it should become:
+
+{
+    "key": 3,
+    "foo.a": 5,
+    "foo.bar.baz": 8
+}
+You can assume keys do not contain dots in them, i.e. no clobbering will occur.
+"""
+
+
+def Dict_Flatten(dictionary, prefix=""):
+    result = {}
+    for key in dictionary.keys():
+        if type(dictionary[key]) is dict:
+            result.update(Dict_Flatten(dictionary[key],prefix+key+'.'))
+        else:
+            result[prefix+key] = dictionary[key]
+            
+    return result
+
+
+test_dict = \
+{
+    "key": 3,
+    "foo": {
+        "a": 5,
+        "bar": {
+            "baz": 8
+        }
+    }
+}
+
+print(Dict_Flatten(test_dict))
