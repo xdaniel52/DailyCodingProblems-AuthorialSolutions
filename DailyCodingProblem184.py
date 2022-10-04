0956aa6388fff8a93d1a184d3f3fb577bee7e468
xdaniel52 <xdaniel@onet.eu>
2022-10-04 13:42:02 +0200

    Add DCP 184

---
+++ DailyCodingProblem184.py
@@ -0,0 +1,29 @@
+"""
+This problem was asked by Amazon.
+
+Given n numbers, find the greatest common denominator between them.
+
+For example, given the numbers [42, 56, 14], return 14
+"""
+
+
+def GCD(arr):
+    denominators = []
+    for i in range(1,arr[0]//2):
+        if arr[0]%i==0:
+            denominators.append(i)
+            
+    for num in arr[1:]:
+        i = 0
+        while i != len(denominators):
+            if num%denominators[i] != 0:
+                denominators.pop(i)
+            else:
+                i+=1
+                     
+    return denominators[-1]
+
+
+
+print(GCD([42, 56, 14]))
+                
