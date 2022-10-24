263d2d1127329d1e00e9e650f05d47674190f9b2
Daniel Kucharski <xdaniel@onet.eu>
2022-10-24 15:21:15 +0200

    Add DCP 214

---
+++ DailyCodingProblem214.py
@@ -0,0 +1,27 @@
+"""
+This problem was asked by Stripe.
+
+Given an integer n, return the length of the longest consecutive run of 1s in its binary representation.
+
+For example, given 156, you should return 3.
+"""
+
+def LenLongestSeqOf1(n):
+    n_str = format(156,"b")
+    max_len = 0
+    counter = 0
+    for num in n_str:
+        if num == "1":
+            counter+=1
+        else:
+            if counter > max_len:
+                max_len = counter
+                counter = 0
+                
+    if counter > max_len:
+        max_len = counter
+        counter = 0
+        
+    return max_len
+    
+print(LenLongestSeqOf1(156))
